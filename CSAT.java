5d2f00c1404b61d63b1cb6585c8276f250030abd
Phonedolly <c1john0803@gmail.com>
2017-08-29 00:26:00 +0900

    Modified to BufferedReader

--- CSAT.java
+++ CSAT.java
@@ -1,14 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 class CSAT{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
 
         System.out.println("대학수학능력시험 영어 영역 단어");
         System.out.println("Q: 단어 퀴즈\nA:단어 외우기");
 
-        Scanner scanOption = new Scanner(System.in);
+        BufferedReader readOption = new BufferedReader(new InputStreamReader(System.in));
 
         //TODO Scanner 사용법 익히기
-        char inputOption = scanOption.toChars();
+        char inputOption = readOption.readLine().toCharArray();
 
         //TDOO Switch Case 형식으로 바꾸기
         //TODO 액션 지정 필요
