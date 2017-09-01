6619678fb5674551b4e42f802d2d7fc75613dca7
Phonedolly <c1john0803@gmail.com>
2017-09-02 08:59:18 +0900

    improve task methods

--- CSAT.java
+++ CSAT.java
@@ -1,31 +1,51 @@
 import java.io.*;
 
 class CSAT{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
             
         int optionMain = onStart(); //옵션 받아오기
+        switch(optionMain){
+            // case 0: {
+            //     MemoryTask.onMemory();
+            //     break;
+            // }
+
+            // case 1: {
+            //     MemoryTask.numBands
+            // }     
+        }
+    }
+
+    private static showIntro(int inputCycle){
+        int introCycle;
+
+        for(introCycle = 0; introCycle <= inputCycle; introCycle++){
+        System.out.println("==================================");
+        }
+
+        System.out.println("대학수학능력시험 영어 영역 단어");
+        System.out.println("Q: 단어 테스트\nA:단어 외우기");
     }
 
     /*인트로*/
     private static int onStart() throws IOException {
+        
+        showIntro();
 
         int returnOption = 7; // 7이 기본값
-
-        System.out.println("대학수학능력시험 영어 영역 단어");
-        System.out.println("Q: 단어 테스트\nA:단어 외우기");
 
         BufferedReader readOption = new BufferedReader(new InputStreamReader(System.in));
 
         //TODO readOption을 어떻게 char로 안전하게 변환할 수 있을까?
-        char inputOption = readOption.readLine().toCharArray();
+        char inputOption = readOption.readLine().charAt(0);
 
         //TDOO Switch Case 형식으로 바꾸기
         //TODO 액션 지정 필요
         switch(inputOption){
-            case A:
+            case 'A':
                 returnOption = 0;
             
-            case Q:
+            case 'Q':
                 returnOption = 1;
         }
 
@@ -33,28 +53,26 @@ class CSAT{
     }
 }
 
-public class Data{
+class Data{
 
     //TODO BufferedReader로 외부 파일 불러오도록 만들기
     String usrName; // 사용자 이름
     String usrMemo; // 사용자 정보
-    int usrCreate; // 생성 시간
+    String usrCreateTime; // 생성 시간
 
     public Data(){
     usrName = "";
     usrMemo = "";
-    usrCreateDate = System.DATETIME;
+    usrCreateTime = "";
     }
 
-    public String getUsrData(boolean def){
-        String[] pushData= {usrName, usrMemo, usrCreate};
+    public String[] getUsrData(boolean def){
+        String[] pushData= {usrName, usrMemo, usrCreateTime};
         return pushData;
     }
-
-
 }
 
-public class MemoryTask{
+class MemoryTask{
     public static void onMemory(){
 
     }
